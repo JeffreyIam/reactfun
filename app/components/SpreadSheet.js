1067023e4ff3b3a992d58af0a150af39c04ee427
Jeff Lam <jefflam1200@gmail.com>
2016-10-13 23:28:05 -0700

    (fix) working editable spreadsheet

--- app/components/SpreadSheet.js
+++ app/components/SpreadSheet.js
@@ -5,8 +5,26 @@ var Spreadsheet = React.createClass({
   getInitialState: function () {
     return {
       headers: range(10).map(entry => 'header'),
-      rows: range(10).map(row => range(10).map(entry => ''))
+      rows: range(10).map(row => range(10).map(entry => '')),
+      editing: null
     }
+  },
+  addInput: function(evt) {
+    evt.preventDefault();
+    console.log(this.input.value)
+  },
+  edit: function (i, j) {
+    this.setState({
+      editing: [i, j],
+    }, () => this.input.focus())
+  },
+  updateCell: function(i, j) {
+    var {rows} = this.state
+    rows[j][i] = this.input.value
+    this.setState({
+      rows: rows,
+      editing: null
+    })
   },
   render: function () {
     return (
@@ -23,10 +41,16 @@ var Spreadsheet = React.createClass({
             </tr>
           </thead>
           <tbody>
-            {this.state.rows.map((row,i) => (
-              <tr key={i}>
-                {[<td key={-1}>{i+1}</td>].concat(row.map((cell,j) =>
-                  <td key={j}></td>
+            {this.state.rows.map((row, j) => (
+              <tr key={j}>
+                {[<td key={-1}>{j+1}</td>].concat(row.map((cell, i) =>
+                  <td key={i}
+                      onMouseUp={() => this.edit(i, j)}>
+                  { this.state.editing && this.state.editing[0] === i && this.state.editing[1] === j
+                    ? <form onSubmit={(e) => this.addInput(e)}>
+                        <input defaultValue={cell} onBlur={() => this.updateCell(i, j)} ref={(cellContent) => {this.input = cellContent}} />
+                      </form>
+                    : cell } </td>
                   ))}
               </tr>
               ))}
